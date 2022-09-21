43448512bd907080efcb781c46d5828a2a7d4070
mayank rajput <55105977+mayankrajput843@users.noreply.github.com>
2022-09-21 17:06:55 +0530

    Add files via upload

---
+++ Assignemntoperator.java
@@ -0,0 +1,23 @@
+package Day2;
+// =  +=   -= /= *= 
+public class Assignemntoperator {
+
+	public static void main(String[] args) {
+		int y;// in java no garbage value
+		
+		y=10;
+		
+		
+		y= y+10;  //y= 20
+		
+		y+=10;
+		
+		y *=10;
+		y++;
+		 
+		System.out.println(y);
+		
+
+	}
+
+}
